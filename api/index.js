7ee49604b4ac64d7f9f9cddf4b8f133b477167c5
Jose Ramirez <developer@trivision.com>
2022-08-08 18:18:17 -0600

    agregada conexion a mogngo atlas

--- api/index.js
+++ api/index.js
@@ -48,10 +48,11 @@ const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'site';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+mongoose.connect('mongodb+srv://humantech:KfeAtiultSQ4DbjC@cluster0.u6yiuhc.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.get('/health', (req, res) => {
